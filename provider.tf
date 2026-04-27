c97e9c0c7104095f262d3cc5201992bdf5739646
arun peri <arunperi@aruns-MacBook-Air.local>
2026-04-27 22:21:03 +0530

    s3

--- provider.tf
+++ provider.tf
@@ -10,3 +10,21 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "devops_s3_31437" {
+  bucket = "devops-s3-31437"
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.devops_s3_31853.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "public_acl" {
+    depends_on = [ aws_s3_bucket_public_access_block.public_access ]
+    bucket = aws_s3_bucket.devops_s3_31853.id
+    acl = "public-read"
+}
